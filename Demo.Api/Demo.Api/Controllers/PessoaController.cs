d9f55b3d4a45480ea3d2374613eb9f8c6f91d08e
martysroka <martysroka@bne.com.br>
2019-05-30 11:48:30 -0300

    inicial

---
+++ Demo.Api/Demo.Api/Controllers/PessoaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(Pessoa._pessoas);
+        }
+
+        [HttpGet("more")]
+        public ActionResult<IEnumerable<string>> LoadMore()
+        {
+            Pessoa.generate(5);
+            return Ok(Pessoa._pessoas);
+        }
+    }
+}
---
+++ Demo.Api/Demo.Data/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo.Data
+{
+    public class Pessoa
+    {
+        public Pessoa(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public static List<Pessoa> _pessoas = new List<Pessoa>();
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public static void generate(int quantidade)
+        {
+            for(int i = 0; i < quantidade;  i++)
+                _pessoas.Add(new Pessoa(_pessoas.Count + 1, $"Nome da pessoa {_pessoas.Count + 1}"));
+        }
+    }
+}
